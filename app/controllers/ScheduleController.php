3bcf08651f8f041262bb3d94de6a4f097f4d1cae
Alan Ly <me@alanly.ca>
2013-04-10 11:56:17 -0400

    Added the filters to `ScheduleController`.
    CHANGES
    * Added the constructor function to `ScheduleController` containing all
    the necessary filters and school session retrieval functionality.

--- app/controllers/ScheduleController.php
+++ app/controllers/ScheduleController.php
@@ -2,6 +2,17 @@
 
 class ScheduleController extends BaseController {
 
+  public function __construct()
+  {
+
+    $this->beforeFilter('csrf', array('on' => 'post'));
+    $this->beforeFilter('auth');
+    $this->beforeFilter('setSchoolSession');
+
+    Session::flash('allSchoolSessions', SchoolSession::all());
+
+  }
+
   public function getIndex()
   {
 
