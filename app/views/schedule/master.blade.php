07e0e9348e4b4494f08cce59b5d4cf06d3f26caa
Alan Ly <me@alanly.ca>
2013-04-10 12:14:10 -0400

    Added School Session selection for Schedules
    CHANGES
    * Added a school session selection box into the Schedules view.

--- app/views/schedule/master.blade.php
+++ app/views/schedule/master.blade.php
@@ -16,6 +16,18 @@ Schedules
       <li{{ Request::is('schedule?') ? ' class="active"' : '' }}><a href="/schedule">List Schedules</a></li>
       <li{{ Request::is('schedule/create*') ? ' class="active"' : '' }}><a href="/schedule/create">Generate a Schedule</a></li>
     </ul>
+    <ul class="nav nav-list well">
+      <li class="nav-header">Current School Session</li>
+      <li>
+        {{ Form::open( array('method' => 'GET', 'class' => 'form-inline', 'id' => 'session_form') ) }}
+          <select id="session" name="session" onchange="$('#session_form').submit()" class="span12">
+            @foreach( Session::get('allSchoolSessions') as $session )
+              <option value="{{ $session->id }}"{{ $session->id == Session::get('schoolSession') ? ' selected' : '' }}>{{{ $session->code }}}</option>
+            @endforeach
+          </select>
+        {{ Form::close() }}
+      </li>
+    </ul>
   </div>
 
   <div class="span10">
