808ff603541b7c7a12cfcac235213749bf60d2b5
Alan Ly <me@alanly.ca>
2013-04-12 02:54:56 -0400

    Created Schedule generation preview view.
    CHANGES
    * The Schedule generation preview view has been created upon Thomas and
    Michael's implementation.

--- app/views/schedule/preview.blade.php
+++ app/views/schedule/preview.blade.php
@@ -27,46 +27,58 @@ Generate a Schedule
       </tr>
     </thead>
 
-    <?php
-
-      $earliest = array(
-        'hour' => substr($earliestTime, 0, (strlen($earliestTime) == 5 ? 2 : 1)),
-        'minute' => substr($earliestTime, (strlen($earliestTime) == 5 ? 3 : 2), 2)
-      );
-
-      $latest = array(
-        'hour' => substr($latestTime, 0, (strlen($latestTime) == 5 ? 2 : 1)),
-        'minute' => substr($latestTime, (strlen($latestTime) == 5 ? 3 : 2), 2)
-      );
-
-      $hour = $earliest['hour'];
-      $minute = 0;
-      $counter = $earliest['minute'] / 15;
-      $totalRows = ((($latest['hour'] * 60) + $latest['minute']) - (($earliest['hour'] * 60) + $earliest['minute'])) / 15;
-
-    ?>
-
     <tbody>
+      <?php
+        $beginTime'hour'] = (strlen($earliest) == 5) ? substr($earliest, 0, 2) : substr($earliest, 0, 1);
+        $beginTime'minute'] = (strlen($earliest) == 5) ? substr($earliest, 3, 2) : substr($earliest, 2, 2);
+        $endTime'hour'] = (strlen($latest) == 5) ? substr($latest, 0, 2) : substr($latest, 0, 1);
+        $endTime'minute'] = (strlen($latest) == 5) ? substr($latest, 3, 2) : substr($latest, 2, 2);
+        $hour = $beginTime'hour'];
+        $minute = 0;
+        $counter = $beginTime'minute'] / 15;
+        $totalRows = ((($endTime'hour'] * 60) + $endTime'minute']) - (($beginTime'hour'] * 60) + $beginTime'minute'])) / 15;
+      ?>
 
-      @for( $i = 0; $i < $totalRows; $i++ )
-
-        <?php 
+      <?php
+        for ($i = 0; $i < $totalRows; $i++) {
           $minute = $counter * 15;
-          $time = $hour . ':' . sprintf('%02d', $minute);
-         ?>
+          $time = "$hour:" . ($minute== 0? "00":$minute);
 
-        <tr>
-          <th>{{{ $time }}}</th>
-          
-          @foreach( $days as $day )
-            @if( isset($day[$time]) )
-              
-            @endif
-          @endforeach
-        </tr>
+          echo "<tr id='$time'>";
+          echo "<th>$time</th>";
 
-      @endfor
-      
+          foreach( $d = 0; $d < 7; $d++ )
+          {
+
+            $day = $days[$d];
+
+            if( isset($day[$time]) ) {
+              $slot = $day[$time];
+              $to_time = strtotime($slot->start_time);
+              $from_time = strtotime($slot->end_time);
+              $rows = round(abs($to_time - $from_time) / 60, 2) / 15;
+
+              echo '<td class="course-block" rowspan="' . $rows . '">' . $slot->course()->code . '<br>' . $slot->type . ' ' . $slot->code . '</td>'.
+
+              $day['no_print_rows'] = $rows - 1;
+            } else if( isset($day['no_print_rows']) && $day['no_print_rows'] > 0  ) {
+              $day['no_print_rows']--;
+            } else {
+              echo "<td></td>";
+            }
+
+          }
+
+          echo "</tr>";
+
+          $counter++;
+
+          if($counter == 4) {
+            $counter = 0;
+            $hour++;
+          }
+        }
+      ?>
     </tbody>
   </table>
 </div>
